c756f9124341f77adb894163bf6d379fe73c8a19
Lehano-A <48220644+Lehano-A@users.noreply.github.com>
2024-05-30 12:22:49 +0300

    Исправляет баг связанный с отсутствием изменения расположения стрелки относительно родительского элемента, когда тот изменяет полюсно своё расположение относительно якоря, в компоненте PopperInterpretation

--- src/components/Main/ModalProduct/PopperInterpretation/PopperInterpretation.jsx
+++ src/components/Main/ModalProduct/PopperInterpretation/PopperInterpretation.jsx
@@ -6,10 +6,83 @@ import { styled } from "@mui/material/styles";
 /* --------------------------------- slices --------------------------------- */
 import { toggleVisiblePopper, setValueInterpretation } from "../../../../redux/reducers/slices/popperInterpretationSlice";
 
+const StyledPopper = styled(Popper, {
+  shouldForwardProp: (prop) => prop !== 'arrow',
+})(({ theme, arrow }) => ({
+  zIndex: 9999999,
+  '& > div': {
+    position: 'relative',
+  },
+
+  '&[data-popper-placement*="bottom"]': {
+    '& > div': {
+      marginTop: arrow ? 2 : 6,
+    },
+    '& .MuiPopper-arrow': {
+      top: 0,
+      left: 0,
+      marginTop: '-5px',
+      width: '3em',
+      height: '1em',
+      '&::before': {
+        borderWidth: '0 6px 6px 6px',
+        borderColor: `transparent transparent black transparent`,
+      },
+    },
+  },
+
+  '&[data-popper-placement*="top"]': {
+    '& > div': {
+      marginBottom: arrow ? 2 : 6,
+    },
+    '& .MuiPopper-arrow': {
+      bottom: 0,
+      left: 0,
+      marginBottom: '-15px',
+      width: '3em',
+      height: '1em',
+      '&::before': {
+        borderWidth: '6px 6px 0 6px',
+        borderColor: `black transparent transparent transparent`,
+      },
+    },
+  },
+
+  '&[data-popper-placement*="right"]': {
+    '& > div': {
+      marginLeft: arrow ? 2 : 6,
+    },
+    '& .MuiPopper-arrow': {
+      left: 0,
+      marginLeft: '-10px',
+      height: '3em',
+      width: '1em',
+      '&::before': {
+        borderWidth: '6px 6px 6px 0',
+        borderColor: `transparent black transparent transparent`,
+      },
+    },
+  },
+
+  '&[data-popper-placement*="left"]': {
+    '& > div': {
+      marginRight: arrow ? 2 : 6,
+    },
+    '& .MuiPopper-arrow': {
+      right: 0,
+      marginRight: '-10px',
+      height: '3em',
+      width: '1em',
+      '&::before': {
+        borderWidth: '6px 0 6px 6px',
+        borderColor: `transparent transparent transparent black`,
+      },
+    },
+  },
+}));
 
 const StyledArrow = styled('div')(() => {
   return {
-    position: 'absolute',
     '&::before': {
       content: '""',
       margin: 'auto',
@@ -17,11 +90,6 @@ const StyledArrow = styled('div')(() => {
       width: '0',
       height: '0',
       borderStyle: 'solid',
-      borderWidth: '5px', // ширина и высота стрелки
-      borderColor: 'transparent transparent black transparent', // цвета границ
-      position: 'absolute',
-      top: '-9px', // смещение стрелки вверх почти на половину ее высоты, и ставим на границу
-      left: '-5px'
     },
   }
 })
@@ -38,7 +106,7 @@ const stylePopperContent = {
 function PopperInterpretation({ refIngredient, interpretationValue }) {
 
   const dispatch = useDispatch()
-  let isVisible = useSelector(state => state.popperInterpretation.visible)
+  const isVisible = useSelector(state => state.popperInterpretation.visible)
   const popperRef = useRef(null);
   const [arrowRef, setArrowRef] = useState(null)
 
@@ -58,50 +126,42 @@ function PopperInterpretation({ refIngredient, interpretationValue }) {
     return () => {
       document.removeEventListener('mousedown', handleClickOutside);
     };
-  }, [dispatch, popperRef, refIngredient]);
-
-
-  const handleArrowRef = (ref) => {
-    setArrowRef(ref);
-  };
+  }, [popperRef, refIngredient]);
 
 
   return (
-    <Popper
+    <StyledPopper
       ref={popperRef}
       transition
       placement="bottom-start"
-      arrow="true"
-      sx={{ zIndex: 9999999 }}
       open={isVisible}
       anchorEl={refIngredient}
-      popperOptions={{
-        modifiers: [
-          {
-            name: "offset",
-            options: {
-              offset: [0, 3],
-            }
+      modifiers={[
+        {
+          name: 'flip',
+          enabled: true,
+          options: {
+            altBoundary: true,
+            rootBoundary: 'document',
+            padding: 8,
           },
-          {
-            name: 'arrow',
-            enabled: true,
-            options: {
-              element: arrowRef,
-            },
+        },
+        {
+          name: 'arrow',
+          enabled: true,
+          options: {
+            element: arrowRef,
           },
-        ]
-      }}
+        },
+      ]}
     >
+
 
       {({ TransitionProps }) => (
         <Fade {...TransitionProps} timeout={{ enter: 200, exit: 0 }}>
           <Box>
 
-            {/* стрелка Popper */}
-            <Box ref={handleArrowRef}>
-              <StyledArrow />
-            </Box>
+            <StyledArrow className="MuiPopper-arrow" ref={setArrowRef} />
 
             <Box sx={stylePopperContent}>
               <Typography sx={{ padding: '15px', fontSize: '14px' }}>
@@ -113,9 +173,10 @@ function PopperInterpretation({ refIngredient, interpretationValue }) {
         </Fade>
       )
       }
-
-    </Popper>
+    </StyledPopper>
   )
 }
 
 export default PopperInterpretation
+
+
