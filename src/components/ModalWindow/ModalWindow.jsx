1c2bd6f1965d34c8622cdd0cfa54460d4b43c776
Lehano-A <48220644+Lehano-A@users.noreply.github.com>
2024-06-20 20:14:10 +0300

    Правит стили и добавляет два пропса - scroll и centeredContent, компоненту ModalWindow

--- src/components/ModalWindow/ModalWindow.jsx
+++ src/components/ModalWindow/ModalWindow.jsx
@@ -9,36 +9,58 @@ import { MEDIA_MD_MODAL_PRODUCT, MEDIA_XS_MODAL_PRODUCT } from "../../helpers/co
 import { selectUserDevice } from "../../redux/reducers/selectors/checkUserDeviceSelectors";
 
 
-const StyledDialog = styled(Dialog, {
-  shouldForwardProp: (prop) => prop !== 'padding' && prop !== 'widthModal' && prop !== 'heightModal' && prop !== 'isVisiblePopperInterpretation' && prop !== 'withoutPaddingScroll' && prop !== 'maxWidth' && prop !== 'breakpoints' && prop !== 'withoutBackdropOverlayOnScroll'
-})(({ padding, widthModal, heightModal, isVisiblePopperInterpretation, withoutPaddingScroll, maxWidth, breakpoints }) => {
+// если модал интерпретации открыт и разрешение .MDPlus, тогда наложим фон поверх модала продукта
+function overlayBGToModalProduct(isVisiblePopperInterpretation, breakpoints) {
 
-  // если модал интерпретации открыт и разрешение .MDPlus, тогда наложим фон поверх модала продукта
-  function overlayBGToModalProduct(isVisiblePopperInterpretation) {
-
-    if (isVisiblePopperInterpretation && breakpoints.MDPlus) {
-      return {
-        '&::after': {
-          display: 'block',
-          content: '""',
-          position: 'absolute',
-          top: 0,
-          right: 0,
-          bottom: 0,
-          left: 0,
-          backgroundColor: 'rgba(0, 0, 0, 0.5)',
-          zIndex: 9999999 - 1, // "9999999" - индекс модала продукта
-        }
+  if (isVisiblePopperInterpretation && breakpoints.MDPlus) {
+    return {
+      '&::after': {
+        display: 'block',
+        content: '""',
+        position: 'absolute',
+        top: 0,
+        right: 0,
+        bottom: 0,
+        left: 0,
+        backgroundColor: 'rgba(0, 0, 0, 0.5)',
+        zIndex: 9999999 - 1, // "9999999" - индекс модала продукта
       }
     }
-    return {}
   }
+  return {}
+}
+
+
+
+function centerContent(centeredContent) {
+  return centeredContent ? {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  } : {}
+}
+
+
+const StyledDialog = styled(Dialog, {
+  shouldForwardProp: (prop) =>
+    prop !== 'padding' &&
+    prop !== 'widthModal' &&
+    prop !== 'heightModal' &&
+    prop !== 'isVisiblePopperInterpretation' &&
+    prop !== 'withoutPaddingScroll' &&
+    prop !== 'maxWidth' &&
+    prop !== 'breakpoints' &&
+    prop !== 'centeredContent'
+})(({ padding, widthModal, heightModal, isVisiblePopperInterpretation, withoutPaddingScroll, maxWidth, breakpoints, centeredContent }) => {
+
+
 
   return {
     // если определять свойства здесь (на верхнем уровне), то это будет относится к backdrop
     '.MuiPaper-root.MuiDialog-paper': {
       padding: padding,
-      ...overlayBGToModalProduct(isVisiblePopperInterpretation),
+      ...overlayBGToModalProduct(isVisiblePopperInterpretation, breakpoints),
+      ...centerContent(centeredContent),
 
       [MEDIA_MD_MODAL_PRODUCT]: {
         marginLeft: withoutPaddingScroll && '17px', // если открывается модал интерпретации, то 17px прибавляется справа из-за наличия скролла, в связи с чем модал не стоит по центру; - это свойство призвано центрировать модал интерпретации
@@ -58,19 +80,19 @@ const StyledDialog = styled(Dialog, {
 
 
 const StyledDialogActions = styled(DialogActions, {
-  shouldForwardProp: (prop) => prop !== 'position' && prop !== 'userDevice'
+  shouldForwardProp: (prop) => prop !== 'positionButtonClose' && prop !== 'userDevice'
 })((props) => {
 
-  const { position, userDevice } = props
+  const { positionButtonClose, userDevice } = props
 
   return {
     zIndex: 10,
     padding: 0,
 
     [MEDIA_XS_MODAL_PRODUCT]: {
-      position: position ? position : 'absolute',
-      top: '4px',
-      right: userDevice === 'desktop' ? '18px' : '2px',
+      position: positionButtonClose ? positionButtonClose : 'absolute',
+      top: '6px',
+      right: userDevice === 'desktop' ? '22px' : '6px',
     },
 
     [MEDIA_MD_MODAL_PRODUCT]: {
@@ -85,7 +107,7 @@ const StyledDialogActions = styled(DialogActions, {
 const StyledButtonClose = styled(IconButton)(() => {
   return {
     backgroundColor: 'transparent',
-    padding: '2px', // область вокруг кнопки визуализируемая при ховере
+    padding: '5px', // область вокруг кнопки визуализируемая при ховере
   }
 })
 
@@ -112,6 +134,8 @@ function ModalWindow(props) {
     withoutPaddingScroll = false,
     hideBackdrop = false,
     breakpoints,
+    scroll = 'body',
+    centeredContent = false
   } = props
 
   const dispatch = useDispatch()
@@ -121,7 +145,7 @@ function ModalWindow(props) {
   const bodyGlobalStyle = <GlobalStyles styles={{ body: { padding: 0, overflow: 'hidden' } }} /> // необходим, иначе на маленьких разрешениях компонентом MUI вносится ненужное инлайновое 'padding-right: 17px' в элемент <body>
 
 
-  function handleClose(e) {
+  function handleClose() {
     setVisible && dispatch(setVisible(false))
 
     if (callbackHandleClose) {
@@ -141,20 +165,21 @@ function ModalWindow(props) {
       heightModal={heightModal}
       maxWidth={maxWidth}
       isVisiblePopperInterpretation={isVisiblePopperInterpretation}
-      scroll='body'
+      scroll={scroll}
       onClose={handleClose}
       aria-labelledby="modal-title"
       open={true}
       fullScreen={isFullScreenModalProduct ? true : false}
       withoutPaddingScroll={withoutPaddingScroll}
       breakpoints={breakpoints}
+      centeredContent={centeredContent}
     >
 
       {bodyGlobalStyle}
       {children}
 
       <StyledDialogActions
-        position={positionButtonClose}
+        positionButtonClose={positionButtonClose}
         userDevice={userDevice}
       >
         <StyledButtonClose color="primary" onClick={handleClose}>
