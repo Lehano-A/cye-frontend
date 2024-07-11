eb0c9f2423db6c2289589861f434914fc3660413
Lehano-A <48220644+Lehano-A@users.noreply.github.com>
2024-07-11 14:53:55 +0300

    Добавляет условие "отсутствия нажатия кнопки пагинации" для устранения перерендера страницы с результатом поиска открывшейся по ссылке

--- src/pages/SearchProductResultPage/SearchProductResultPage.jsx
+++ src/pages/SearchProductResultPage/SearchProductResultPage.jsx
@@ -1,7 +1,7 @@
 import { useEffect } from "react"
 import queryString from 'query-string'
 import loglevel from 'loglevel'
-import { Outlet, createSearchParams, useLocation, useNavigationType, useParams } from "react-router-dom"
+import { Outlet, useLocation, useNavigationType, useParams } from "react-router-dom"
 import useSendingReqToApi from "../../hooks/useSendingReqToApi"
 import { useDispatch, useSelector } from "react-redux"
 import { startLoadingIndicatorBoxSearchResult } from "../../redux/reducers/actions/BoxSearchResult/loadingIndicatorActions"
@@ -28,11 +28,17 @@ function SearchProductResultPage({ ErrorComponent }) {
   const isRedirectionFromModalProductPage = useSelector((state) => state.navigation.isRedirectionFromModalProductPage)
   const isVisibleModalProduct = useSelector((state) => state.modalProduct.isVisibleModalProduct)
   const apiFoundProductsAfterSubmit = useSelector((state) => state.searchRequestProduct.apiFoundProductsAfterSubmit)
+  const isPressedButtonPagination = useSelector((state) => state.pagination.isPressedButtonPagination)
+
 
   useEffect(() => {
-    // когда произошло движение по истории НАЗАД или ВПЕРЁД, или ОБНОВИЛАСЬ страница, или страница ОТКРЫЛАСЬ ПО ССЫЛКЕ
-    if (navigationType === 'POP' || (navigationType === 'REPLACE' && isRedirectionFromModalProductPage)) {
-      log.debug(`
+
+    // если нет нажатия на кнопку пагинации
+    if (!isPressedButtonPagination) {
+
+      // когда произошло движение по истории НАЗАД или ВПЕРЁД, или ОБНОВИЛАСЬ страница, или страница ОТКРЫЛАСЬ ПО ССЫЛКЕ
+      if (navigationType === 'POP' || (navigationType === 'REPLACE' && isRedirectionFromModalProductPage)) {
+        log.debug(`
       Открылась страница "SearchProductResultPage"
       Тип действия: движение по истории НАЗАД или ВПЕРЁД, или ОБНОВИЛАСЬ страница, или страница ОТКРЫЛАСЬ ПО ССЫЛКЕ
 
@@ -40,55 +46,46 @@ function SearchProductResultPage({ ErrorComponent }) {
       search: ${location.search}
       params: `, params)
 
-      dispatch(startLoadingIndicatorBoxSearchResult())
+        dispatch(startLoadingIndicatorBoxSearchResult())
 
-      if (isRedirectionFromModalProductPage) {
-        dispatch(setIsRedirectionFromModalProductPage(false))
+        if (isRedirectionFromModalProductPage) {
+          dispatch(setIsRedirectionFromModalProductPage(false))
+        }
+
+        const parsedQueryParams = queryString.parse(location.search)
+        let createdQueryParams
+
+        // если в поисковых параметрах есть "searchBy" с значением - "text", "brand" или "category"
+        // тогда поиск нужно производить исключительно по этим параметрам
+        if (
+          parsedQueryParams.searchBy === "text" ||
+          parsedQueryParams.searchBy === "brand" ||
+          parsedQueryParams.searchBy === "category"
+        ) {
+          createdQueryParams = queryParams.create({ withoutDynamicParams: true })
+        } else {
+          createdQueryParams = queryParams.create()
+        }
+
+        sendingReqToApi.findProduct({
+          stage: MOVEMENT_BY_HISTORY_UPDATE_PAGE_OR_FOLLOWED_LINK,
+          apiMethod: api.findProduct,
+          searchData: createdQueryParams,
+          segmentSearch: parsedQueryParams.searchBy || params.searchBy || BRAND_AND_CATEGORY
+        })
+        return
       }
 
-      const parsedQueryParams = queryString.parse(location.search)
-      let createdQueryParams
-
-      // если в поисковых параметрах есть "searchBy" с значением - "text", "brand" или "category"
-      // тогда поиск нужно производить исключительно по этим параметрам
-      if (
-        parsedQueryParams.searchBy === "text" ||
-        parsedQueryParams.searchBy === "brand" ||
-        parsedQueryParams.searchBy === "category"
-      ) {
-        createdQueryParams = queryParams.create({ withoutDynamicParams: true })
-      } else {
-        createdQueryParams = queryParams.create()
-      }
-
-      sendingReqToApi.findProduct({
-        stage: MOVEMENT_BY_HISTORY_UPDATE_PAGE_OR_FOLLOWED_LINK,
-        apiMethod: api.findProduct,
-        searchData: createdQueryParams,
-        segmentSearch: parsedQueryParams.searchBy || params.searchBy || BRAND_AND_CATEGORY
-      })
-      return
-
-
-      console.log('createdQueryParams', createdQueryParams);
-      sendingReqToApi.findProduct({
-        stage: MOVEMENT_BY_HISTORY_UPDATE_PAGE_OR_FOLLOWED_LINK,
-        apiMethod: api.findProduct,
-        searchData: createdQueryParams,
-        segmentSearch: params.searchBy || BRAND_AND_CATEGORY
-      })
-      return
-    }
-
-    // если происходит "классический поиск" через поле поиска
-    log.debug(`
+      // если происходит "классический поиск" через поле поиска
+      log.debug(`
       Открылась страница "SearchProductResultPage"
       Тип действия: "классический поиск" через поле поиска
 
       navigationType: ${navigationType}
       search: ${location.search}
       params: `, params
-    )
+      )
+    }
   }, [window.history.state.idx])
 
 
