8f4bcba049e1737b57eb1e5efc4d4f4fab9e66a4
Lehano-A <48220644+Lehano-A@users.noreply.github.com>
2024-07-16 14:40:20 +0300

    Добавляет условия проверки на "консервант" в стили и пропсы компонента CompositeIngredient

--- src/components/Main/ModalProduct/Ingredients/CompositeIngredient/CompositeIngredient.jsx
+++ src/components/Main/ModalProduct/Ingredients/CompositeIngredient/CompositeIngredient.jsx
@@ -2,22 +2,24 @@ import React from "react";
 import { Box, Paper, Typography } from "@mui/material"
 import Ingredient from "../Ingredient/Ingredient"
 import { styled } from "@mui/material/styles";
+import { PRESERVING_AGENTS_TYPE_ATTENTION_ICON } from "../../../../../helpers/constants";
 
-const StyledMainBox = styled(Paper)(() => {
+const StyledMainBox = styled(Paper, { shouldForwardProp: (prop) => prop !== 'isPreservingAgents' })(({ isPreservingAgents, theme }) => {
   return {
-    backgroundColor: 'rgba(0, 0, 0, 0.03)',
     margin: '15px 0',
     padding: '15px 20px',
-    borderRadius: '15px'
+    borderRadius: '15px',
+    backgroundColor: isPreservingAgents ? theme.palette.getAlphaColor(PRESERVING_AGENTS_TYPE_ATTENTION_ICON, "light", 0.15) : 'rgba(0, 0, 0, 0.03)'
   }
 })
 
 
-const StyledTitle = styled(Typography)(() => {
+const StyledTitle = styled(Typography, { shouldForwardProp: (prop) => prop !== 'isPreservingAgents' })(({ isPreservingAgents, theme }) => {
   return {
     fontWeight: 700,
     fontStyle: 'italic',
-    margin: '0 0 5px 0'
+    margin: '0 0 5px 0',
+    color: isPreservingAgents ? theme.palette.preservingAgents.main : 'initial'
   }
 })
 
@@ -25,9 +27,18 @@ const StyledTitle = styled(Typography)(() => {
 
 function CompositeIngredient({ data, setRefSelectedIngredient }) {
 
+  const isPreservingAgents = data.composition[0].ingredient?.type.includes(PRESERVING_AGENTS_TYPE_ATTENTION_ICON)
+
+
   return (
-    <StyledMainBox elevation={1}>
-      <StyledTitle variant="body2">
+    <StyledMainBox
+      elevation={1}
+      isPreservingAgents={isPreservingAgents}
+    >
+      <StyledTitle
+        variant="body2"
+        isPreservingAgents={isPreservingAgents}
+      >
         {data.title + ':'}
       </StyledTitle>
 
